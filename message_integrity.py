3801344aa57f4759fa3b5b107fcad42f701695a6
Michael Stark <michaelstark@Michaels-MacBook-Pro.local>
2026-04-24 11:11:03 -0400

    print hashed string

--- message_integrity.py
+++ message_integrity.py
@@ -6,9 +6,8 @@ content = input("Enter a word to be encrypted \n==> ")
 hash_object = hashlib.sha256(content.encode('utf-8'))
 hex_dig = hash_object.hexdigest()
 
-# commented out the print function so the hash is not visible
-# would delete in real world setting but leaving in as a reminder to myself to remove print functions after using to check things smh
-# print(f"Hashed String: {hex_dig}")
+
+print(f"Hashed String: {hex_dig}")
 
 # Original hashed content storage
 original_hash = hex_dig
@@ -19,6 +18,6 @@ verify_hash = hashlib.sha256(verify_data.encode('utf-8')).hexdigest()
 
 # Compare the hashes
 if verify_hash == original_hash:
-    print("Integrity verified, message not altered, you did good today son")
+    print("Integrity verified, message not altered!")
 else:
     print("Dangit! Someone has been tampering the with the code!")
